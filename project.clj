111563d981fe5c7cd73466ee1ee44e3be9636196
Nicolas Modrzyk <hellonico@gmail.com>
2016-05-05 09:41:33 +0900

    updated core async and project name

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject com.gearswithingears/async-sockets "0.1.0"
+(defproject com.hellonico.gearswithingears/async-sockets "0.1.0"
   :description "A Clojure library for working with sockets using core.async channels."
   :url "https://github.com/bguthrie/async-sockets"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/core.async "0.1.346.0-17112a-alpha"]
+  :dependencies [[org.clojure/core.async "0.2.374"]
                  [org.clojure/tools.logging "0.3.1"]]
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]
                                   [clj-time "0.8.0"]]
